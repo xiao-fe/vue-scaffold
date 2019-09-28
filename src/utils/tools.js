4822df908180dfc1c69f67c2442e480610896bf7
lvxiaoxiao <lvxiaoxiao@daojia-inc.com>
2019-09-28 18:08:03 +0800

    添加函数节流

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -71,6 +71,43 @@ function debounce(fn, wait, immediate) {
   return debounced
 }
 
+/**
+ * @desc 函数节流
+ * @param func 函数
+ * @param wait 延迟执行毫秒数
+ * @param type 1 表时间戳版，2 表定时器版
+ */
+function throttle (func, wait ,type) {
+  if(type === 1){
+    let previous = 0;
+  } else if(type === 2){
+    let timeout;
+  }
+  return function() {
+    let context = this;
+    let args = arguments;
+    if(type === 1) {
+      let now = Date.now();
+      if (now - previous > wait) {
+        func.apply(context, args);
+        previous = now;
+      }
+    }else if(type === 2) {
+      if (!timeout) {
+        timeout = setTimeout(() => {
+          timeout = null;
+          func.apply(context, args)
+        }, wait)
+      }
+    }
+  }
+}
+
+/**
+ * 时间戳转换日期
+ * @param time
+ * @returns {string}
+ */
 function changeDateFormat(time) {
   if (time.length === 10) time = Number(time) * 1000;
   let now = new Date(time);
@@ -121,3 +158,7 @@ export {
   exitFullScreen,
   openFullScreen
 }
+
+
+
+
